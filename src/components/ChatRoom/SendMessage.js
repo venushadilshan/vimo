a86abd5b83bd2099a693d78e1ff351cb3dafc46e
Venusha <venusha125@gmail.com>
2022-01-23 11:25:28 +0530

    Create chatItem, SendMessage

--- src/components/ChatRoom/SendMessage.js
+++ src/components/ChatRoom/SendMessage.js
@@ -2,7 +2,7 @@ import {MdSend} from "react-icons/md";
 const SendMessage = () => {
     return (<div className="flex flex-row w-full bg-gray-800 items-center absolute bottom-0 px-5 py-2 ">
 
-        <input type="text" className="text-sm h-8 p-2 w-full outline-none rounded-full" placeholder="Send something" rows={1}/>
+        <input type="text" className="text-sm h-8 p-2 w-full outline-none bg-gray-500 text-slate-50 rounded-full" placeholder="Send something" rows={1}/>
         <button className="bg-blue-500 mx-5 h-8 w-auto items-center p-2 text-slate-50 rounded-full flex flex-row"><MdSend/> </button>
     </div>);
 }
--- src/components/ChatRoom/TitleBar.js
+++ src/components/ChatRoom/TitleBar.js
@@ -2,9 +2,17 @@ import ProfileIcon from "./ProfileIcon";
 
 const TitleBar = () => {
     return (
-    <div className="flex flex-row w-full bg-gray-800 items-center fixed">
+    <div className="flex flex-row w-full bg-gray-800 items-center absolute justify-between px-2">
+        <div className="flex flex-row items-center">
         <ProfileIcon/>
-        <h1 className="text-lg text-slate-50 p-2">Venusha Dilshan</h1>
+        <div className="flex flex-col items-start ">
+        <h1 className="text-lg text-slate-50 ml-2">Venusha Dilshan</h1>
+        <p className="text-sm text-slate-400 ml-3" style={{fontSize:"0.6em"}}>Venusha, Dilshan</p>
+        </div>
+        </div>
+       
+        <input type="text" className="mr-3 w-50 rounded-full bg-gray-500 outline-none text-slate-50 text-sm p-1" placeholder="Search this chat"/>
+       
     </div>);
 }
 
