a6227253ee0ac2b800cb8e410a2b340699d46fb9
Venusha <venusha125@gmail.com>
2022-01-23 10:52:58 +0530

    Create chatItem, SendMessage

--- src/components/ChatRoom/ChatItem.js
+++ src/components/ChatRoom/ChatItem.js
@@ -6,10 +6,14 @@ const ChatItem = () => {
             <div className="flex flex-col">
                 <ProfileIcon />
             </div>
-            <div className="flex flex-col h-auto w-fill text-slate-50">
-                <h1 className="font-semibold font-md">Dilshan</h1>
-                <p className="text-md">Meet me at the gym</p>
-                <p className="text-sm">9.00AM 20/01/2020</p>
+            <div className="flex flex-col h-auto w-full text-slate-50 ml-2">
+                <div className="flex flex-row w-full justify-start items-center">
+                <h1 className="font-semibold text-sm">Dilshan</h1>
+                <p className="text-sm text-slate-400 ml-2" style={{fontSize:"0.6em"}}>9.00AM 20/01/2020</p>
+                </div>
+             
+                <p className="text-sm w-4/5">Meet me at the gym </p>
+               
             </div>
         </div>);
 }
--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -1,11 +1,13 @@
 import ChatContainer from "./ChatContainer";
 import ChatItem from "./ChatItem";
+import SendMessage from "./SendMessage";
 import TitleBar from "./TitleBar";
 const ChatRoom = () => {
     return (
     <div className="container w-full relative">
         <TitleBar/>
         <ChatContainer/>
+        <SendMessage/>
     </div>);
 }
 
